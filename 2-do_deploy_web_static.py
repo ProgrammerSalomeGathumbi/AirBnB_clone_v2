4561fd5561786675fafde584d7224d8570900b66
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-05 15:48:39 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,18 +21,17 @@ def do_pack():
         return ("versions/web_static_{}.tgz".format(t))
     except Exception as e:
         return None
-	
+
 
 def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
     """
     if os.path.exists(archive_path):
-        arch_file = archive_path.split('/')[1]
-        f_file = archive_path.split('.')[0]
-        folder = "/data/web_static/releases/{}".format(f_file)
-        arch = "/tmp/{}".format(arch_file)
-        put(archive_path, arch)
+        arch_file = archive_path[9:]
+        folder = "/data/web_static/releases/{}" + arch_file[:-4]
+        arch_file = "/tmp/" + arch_file
+        put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(folder))
         run("sudo tar -xzf {} -C {}/".format(arch_file, folder))
         run("sudo rm {}".format(arch_file))
