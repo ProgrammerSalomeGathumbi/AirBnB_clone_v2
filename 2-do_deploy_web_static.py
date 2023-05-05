2d09717a86a36dc896447f2277b79f97e25aa5c1
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-05 14:33:21 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,9 @@ Fabric script that distributes an archive
 import os
 from fabric.api import *
 
-
 env.hosts = ["100.24.255.168", "100.25.161.76"]
+env.user = "ubuntu"
+env.key = "/root/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
@@ -14,9 +15,10 @@ def do_deploy(archive_path):
     Distributes an archive to your web servers
     """
     if os.path.exists(archive_path):
-        put(archive_path, "/tmp/")
-        arch_file = os.path.basename(archive_path)
+        arch_file = archive_path[9:]
         folder = "/data/web_static/releases/" + arch_file[:-4]
+        arch_file = "/tmp/" + arch_file
+        put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(folder))
         run("sudo tar -xzf {} -C {}/".format(arch_file, folder))
         run("sudo rm {}".format(arch_file))
@@ -24,5 +26,6 @@ def do_deploy(archive_path):
         run("sudo rm -rf {}/web_static".format(folder))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(folder))
+        print("New version deployed!")
         return (True)
     return (False)
