1d3a0ba96183ea878296790c6a902423aefbaf0e
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-05 14:49:22 +0300

    3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Fabric script that created & distributes an archive
+"""
+from datetime import datetime
+import os
+from fabric.api import *
+
+env.hosts = ["100.24.255.168", "100.25.161.76"]
+env.user = "ubuntu"
+env.key = "/root/.ssh/id_rsa"
+
+
+def do_pack():
+    """
+    Generates a .tgz archive
+    """
+    try:
+        local("mkdir -p versions")
+        t = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(t))
+        return ("versions/web_static_{}.tgz".format(t))
+    except Exception as e:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to your web servers
+    """
+    if os.path.exists(archive_path):
+        arch_file = archive_path[9:]
+        folder = "/data/web_static/releases/" + arch_file[:-4]
+        arch_file = "/tmp/" + arch_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(folder))
+        run("sudo tar -xzf {} -C {}/".format(arch_file, folder))
+        run("sudo rm {}".format(arch_file))
+        run("sudo mv {}/web_static/* {}".format(folder, folder))
+        run("sudo rm -rf {}/web_static".format(folder))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(folder))
+        print("New version deployed!")
+        return (True)
+    return (False)
+
+
+def deploy():
+    """
+    creates and distributes an archive to your web servers
+    """
+    file = do_pack()
+    if file is None:
+        return (False)
+    return (do_deploy(file))
