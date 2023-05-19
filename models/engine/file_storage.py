757fd18404a83f3b84f721c7b4aeda2e179083f2
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-19 16:09:16 +0300

    Updated files in the model folder

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -10,14 +17,15 @@ class FileStorage:
 
     def all(self, cls=None):
         """ returns the list of objects of one type of class."""
-        if cls is None:
-            return self.__objects
-        else:
-            new_obj = {}
-            for key, value in self.__objects.items():
-                if value.__class__ == cls:
-                    new_obj[key] = value
-        return new_obj
+        if cls:
+            same_type = dict()
+
+            for key, obj in self.__objects.items():
+                if obj.__class == cls:
+                    same_type[key] = obj
+
+            return same_type
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -34,25 +42,11 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding='UTF-8') as f:
+                for key, val in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
             pass
 
--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="states", cascade="all, delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ return the list of City objects"""
+            cities = models.storage.all(City)
+            city_list = []
+            for city in cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
