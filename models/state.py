19811e13622a870325b919ad3fffe7be954129c9
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-22 08:04:00 +0300

    Fixed code

--- models/state.py
+++ models/state.py
@@ -11,18 +11,19 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", backref="states",
                               cascade="all, delete")
+        name = Column(String(128), nullable=False)
     else:
         @property
         def cities(self):
             """ return the list of City objects"""
-            cities = storage.all(City)
-            city_list = []
-            for city in cities.values():
+            cities = list()
+
+            for _id, city in models.storage.all(City).items():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    cities.append(city)
+
+            return cities
